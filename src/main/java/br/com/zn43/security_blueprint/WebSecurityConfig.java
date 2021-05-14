eb9903565af14c6cbf690296c648183f0b226e1a
Felipe Zambrin <zambrinf@gmail.com>
2021-05-13 22:49:36 -0300

    WebSecurity configuration

---
+++ src/main/java/br/com/zn43/security_blueprint/WebSecurityConfig.java
@@ -0,0 +1,34 @@
+package br.com.zn43.security_blueprint;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("admin")
+                .password(passwordEncoder.encode("admin"))
+                .roles("ADMIN", "USER")
+                .and()
+                .withUser("user")
+                .password(passwordEncoder.encode("user"))
+                .roles("USER");
+    }
+
+    @Bean
+    public PasswordEncoder getPassowrdEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
