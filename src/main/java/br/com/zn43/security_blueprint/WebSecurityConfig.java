2790d28a68cb165a7f474c80bcdddd4fb2ae1091
Felipe Zambrin <zambrinf@gmail.com>
2021-05-13 23:28:37 -0300

    Comentarios

--- src/main/java/br/com/zn43/security_blueprint/WebSecurityConfig.java
+++ src/main/java/br/com/zn43/security_blueprint/WebSecurityConfig.java
@@ -15,7 +15,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private PasswordEncoder passwordEncoder;
 
-    // AUTHENTICATION
+    // AUTHENTICATION MANAGER
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
